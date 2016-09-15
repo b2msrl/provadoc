1e1e9ae530e122e7b17f3b27f6e48ba634cb2921
alevento <alessandro.manneschi@gmail.com>
2016-09-15 14:49:26 +0200

    Substitute

--- sami_config.php
+++ sami_config.php
@@ -19,10 +19,10 @@ $versions = GitVersionCollection::create($dir)
 
 return new Sami($iterator,array(
     'theme'                => 'default',
-    'title'                => '@@@package_name API',
+    'title'                => 'provadoc API',
     'versions'             => $versions,
-    'build_dir'            => '@@@doc_destination/build/%version%',
-    'cache_dir'            => '@@@doc_destination/cache/%version%',
+    'build_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/provacdoc/build/%version%',
+    'cache_dir'            => 'Y:/Public/laravel-packages/www/doc/b2msrl/provacdoc/cache/%version%',
     'default_opened_level' => 2,
 ));
 
